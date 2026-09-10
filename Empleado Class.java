e5110232e3010b685afc0bc2e5e6685f54b29182
davidcuecaaa-code <davidcuecaaa@gmail.com>
2026-09-10 15:26:39 -0500

    Update Empleado Class.java

--- Empleado Class.java
+++ Empleado Class.java
@@ -1,16 +1,31 @@
 package epec;
-
 public class Empleado {
-    // Atributos comunes (ej. nombre, id, salario)
+    
     protected String nombre;
     protected double salario;
 
     public Empleado() { 
-        // Constructor base
     }
+
 
     public Empleado(String nombre, double salario) {
         this.nombre = nombre;
+        this.salario = salario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
         this.salario = salario;
     }
 }
