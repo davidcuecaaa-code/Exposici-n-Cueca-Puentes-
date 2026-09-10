a0312b70db6cae68d18b52c2370d272a65e787b6
davidcuecaaa-code <davidcuecaaa@gmail.com>
2026-09-10 15:02:56 -0500

    Update Contador Class.java

--- Contador Class.java
+++ Contador Class.java
@@ -2,12 +2,11 @@ package epec;
 
 public class Contador extends Empleado {
 
-    // Constructor por defecto
     public Contador() {
         super();
     }
 
-    // Constructor parametrizado
+    
     public Contador(String nombre, double salario) {
         super(nombre, salario);
     }
