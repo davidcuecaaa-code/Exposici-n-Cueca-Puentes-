3f8bf754f117444f5004ccb55d079b6e6387388e
davidcuecaaa-code <davidcuecaaa@gmail.com>
2026-09-10 15:03:20 -0500

    Update Empleado Class.java

--- Empleado Class.java
+++ Empleado Class.java
@@ -1,21 +1,21 @@
 package epec;
 
 public class Empleado {
-    // Atributos protegidos para permitir el acceso en subclases
+    
     protected String nombre;
     protected double salario;
 
-    // Constructor por defecto
+   
     public Empleado() { 
     }
 
-    // Constructor parametrizado
+   
     public Empleado(String nombre, double salario) {
         this.nombre = nombre;
         this.salario = salario;
     }
 
-    // Getters y Setters
+   
     public String getNombre() {
         return nombre;
     }
